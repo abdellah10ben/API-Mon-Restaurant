a3c3d5baa0cb901c8af8e740f07ad3074e62e26b
Bouyanzer <mohammed.bouyanzer99@gmail.com>
2026-01-05 00:37:41 +0100

    Finalisation du projet avec la classe principale de l'application

---
+++ src/main/java/com/bouyanzer/MonRestaurantApplication.java
@@ -0,0 +1,11 @@
+package com.bouyanzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonRestaurantApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonRestaurantApplication.class, args);
+	}
+}
